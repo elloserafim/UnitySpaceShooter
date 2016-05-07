f78eb2ce2ba0c2ef108b637bbbbf733a1d282362
elloserafim <elloserafim89@gmail.com>
2016-05-07 17:34:58 -0300

    Initial

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+    void OnTriggerExit(Collider other)
+    {
+        // Destroy everything that leaves the trigger
+		if (other.name != "Player") {
+			Destroy (other.gameObject);
+		}
+    }
+}
+ 
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float tilt;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate;
+    private float nextFire;
+
+
+    public Boundary boundary;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            //GameObject clone = 
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			Debug.Log (GetComponent<AudioSource>());
+			GetComponent<AudioSource>().Play();
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.xMin, boundary.xMax));
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, (GetComponent<Rigidbody>().velocity.x * -tilt));
+    }
+
+
+}
